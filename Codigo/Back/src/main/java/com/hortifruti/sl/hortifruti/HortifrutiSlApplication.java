6ba71bba4d980ee811932968510b1a45720f1baf
Marcos Alberto <159904717+marcosffp@users.noreply.github.com>
2025-08-29 08:28:18 -0300

    Adiciona estrutura inicial do projeto Spring Boot com configuração do Maven e classes principais

---
+++ Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/HortifrutiSlApplication.java
@@ -0,0 +1,13 @@
+package com.hortifruti.sl.hortifruti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortifrutiSlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortifrutiSlApplication.class, args);
+	}
+
+}
---
+++ Codigo/Back/src/test/java/com/hortifruti/sl/hortifruti/HortifrutiSlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hortifruti.sl.hortifruti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortifrutiSlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
