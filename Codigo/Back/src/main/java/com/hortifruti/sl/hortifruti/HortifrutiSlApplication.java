b9080e8c7c8de89569b502a44202221a80506a2b
Marcos Alberto <159904717+marcosffp@users.noreply.github.com>
2025-08-31 18:38:09 -0300

    feat: Remove chamada ao método SpringApplication.run na classe HortifrutiSlApplication

--- Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/HortifrutiSlApplication.java
+++ Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/HortifrutiSlApplication.java
@@ -6,6 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HortifrutiSlApplication {
   public static void main(String[] args) {
-    SpringApplication.run(HortifrutiSlApplication.class, args);
-  }
+  
 }
