522b8f3f5b55c8970ff38a68afa2d78d1118c7ce
Marcos Alberto <159904717+marcosffp@users.noreply.github.com>
2025-08-31 18:06:59 -0300

    feat: Remove método main da classe HortifrutiSlApplication

--- Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/HortifrutiSlApplication.java
+++ Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/HortifrutiSlApplication.java
@@ -5,7 +5,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HortifrutiSlApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(HortifrutiSlApplication.class, args);
-  }
-}
+  publ
