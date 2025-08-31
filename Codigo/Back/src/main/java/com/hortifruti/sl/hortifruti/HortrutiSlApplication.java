06780c35c53493c780ef4c36f2c42a400870e426
Marcos Alberto <159904717+marcosffp@users.noreply.github.com>
2025-08-31 18:07:39 -0300

    feat: Restaura a classe HortifrutiSlApplication removida

--- Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/HortrutiSlApplication.java
+++ Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/HortrutiSlApplication.java
@@ -4,5 +4,5 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HortifrutiSlApplication {
+public class HortrutiSlApplication {
   publ
