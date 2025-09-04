2fd7fb5c132860ed37448d2a4ec9afff3fdbed48
Rafael Moura <rafaelganascinidemoura@gmail.com>
2025-09-04 15:14:42 -0300

    feat: adicionar mapeamento e repositório para a entidade Client

---
+++ Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/mapper/ClientMapper.java
@@ -0,0 +1,22 @@
+package com.hortifruti.sl.hortifruti.mapper;
+
+import com.hortifruti.sl.hortifruti.dto.ClientRequest;
+import com.hortifruti.sl.hortifruti.dto.ClientResponse;
+import com.hortifruti.sl.hortifruti.model.Client;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ClientMapper {
+
+    @Mapping(target = "id", ignore = true)
+    Client toClient(ClientRequest clientRequest);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "clientName", target = "clientName")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "phoneNumber", target = "phoneNumber")
+    @Mapping(source = "address", target = "address")
+    ClientResponse toClientResponse(Client client);
+}
