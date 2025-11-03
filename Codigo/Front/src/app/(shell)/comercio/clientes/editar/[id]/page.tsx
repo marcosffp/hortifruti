0697a0f1b45104e3e67e4a5a8ac7e51d29326fae
marcosffp <mafpinto@sga.pucminas.br>
2025-11-03 07:21:20 -0300

    fix: simplify parameter handling in EditarClientePage component

--- Codigo/Front/src/app/(shell)/comercio/clientes/editar/[id]/page.tsx
+++ Codigo/Front/src/app/(shell)/comercio/clientes/editar/[id]/page.tsx
@@ -1,11 +1,11 @@
 import EditarClientePageContent from "./content";
 
 interface PageProps {
-  readonly params: { id: string; } | Promise<{ id: string; }>;
+  readonly params: { id: string; };
 }
 
 export default async function EditarClientePage({ params }: PageProps) {
-  const { id } = await Promise.resolve(params);
+  const { id } = params;
   
   return <EditarClientePageContent id={id} />;
 }
