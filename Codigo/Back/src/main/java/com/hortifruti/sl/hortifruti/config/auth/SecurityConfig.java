f03528d719591cb0be2a8592c4ca3163091d5211
Marcos Alberto Ferreira Pinto <159904717+marcosffp@users.noreply.github.com>
2025-11-09 20:46:47 -0300

    Update SecurityConfig.java

--- Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/config/auth/SecurityConfig.java
+++ Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/config/auth/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
             auth ->
-                auth.requestMatchers("/auth", "/swagger-ui/**", "/v3/api-docs/**", "/scheduler/**")
+                auth.requestMatchers("/auth", "/swagger-ui/**", "/v3/api-docs/**", "/scheduler/**","/backup/oauth2callback")
                     .permitAll()
                     .requestMatchers(org.springframework.http.HttpMethod.GET, "/clients/**")
                     .permitAll()
