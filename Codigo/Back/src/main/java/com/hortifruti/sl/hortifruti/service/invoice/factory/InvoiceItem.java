972f758a8c00efbdacb74f04863de72260563f2f
marcosffp <mafpinto@sga.pucminas.br>
2026-04-13 00:12:58 -0300

    Format total value in InvoiceItem to two decimal places

--- Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/service/invoice/factory/InvoiceItem.java
+++ Codigo/Back/src/main/java/com/hortifruti/sl/hortifruti/service/invoice/factory/InvoiceItem.java
@@ -37,7 +37,7 @@ public class InvoiceItem {
               (String) productData.get("unidade_comercial"),
               product.getQuantity(),
               product.getPrice(),
-              product.getTotalValue(),
+              product.getTotalValue().setScale(2, java.math.RoundingMode.HALF_UP),
               (String) productData.get("unidade_tributavel"),
               product.getQuantity(),
               product.getPrice(),
