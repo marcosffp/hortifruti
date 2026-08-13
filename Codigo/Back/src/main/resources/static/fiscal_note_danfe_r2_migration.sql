f99a38438c3991dafc9b5605e52d63cd3ebe1004
Marcos Alberto <marcosalbertoferreirapintoalbe@gmail.com>
2026-08-13 14:34:40 -0300

    feat: remove obsolete migration scripts for database schema updates

--- Codigo/Back/src/main/resources/static/fiscal_note_danfe_r2_migration.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Add DANFE (PDF) storage column to fiscal_note_xml_storage
--- Run this script once in your database (hml/prod).
--- New rows will have danfe_object_key populated pointing to the R2 object;
--- rows created before this migration will have it NULL until the DANFE is
--- fetched/downloaded once, at which point it's lazily saved (see
--- FiscalNoteXmlStorageService.saveDanfeIfAbsent).
-
-ALTER TABLE fiscal_note_xml_storage
-    ADD COLUMN danfe_object_key VARCHAR(500) NULL;
--- Codigo/Back/src/main/resources/static/statement_origin_backfill.sql
+++
@@ -1,6 +0,0 @@
--- Backfill: marca extratos existentes (antes da coluna "origin" existir) como PDF_UPLOAD.
--- Run this script once in your database (hml/prod) after the app has started at least once
--- with Hibernate ddl-auto=update (which creates the "origin" column automatically).
--- Non-destructive: only fills rows where origin is still NULL.
-
-UPDATE statements SET origin = 'PDF_UPLOAD' WHERE origin IS NULL;
